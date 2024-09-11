c8f0e75faf36ad5f2b627ef24b7146a45e19df58
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2024-09-11 06:16:11 +0100

    feat(app): 111 Add gravity as a props

--- src/components/Physics.vue
+++ src/components/Physics.vue
@@ -1,18 +1,39 @@
 <script setup lang="ts">
+import { watch } from 'vue'
 import { useLoop } from '@tresjs/core'
 
+import type { TresVector3 } from '@tresjs/core'
+import { Vector3 } from 'three'
 import { useRapierContextProvider } from '../composables/useRapier'
-import type { PhysicsProps } from '../types/physics'
+// import type { PhysicsProps } from '../types/physics'
+import { GRAVITY } from '../constants/physics'
 import Debug from './Debug.vue'
 
-withDefaults(
+export interface PhysicsProps {
+  debug: boolean
+  gravity: TresVector3
+}
+
+const props = withDefaults(
   defineProps<Partial<PhysicsProps>>(),
   {
+    gravity: () => new Vector3(GRAVITY.x, GRAVITY.y, GRAVITY.z),
     debug: false,
   },
 )
 
 const { world } = await useRapierContextProvider()
+
+const setGravity = (gravity: TresVector3) => {
+  world.gravity.x = gravity.x ?? gravity[0]
+  world.gravity.y = gravity.y ?? gravity[1]
+  world.gravity.z = gravity.z ?? gravity[2]
+}
+
+watch(() => props.gravity, (gravity) => {
+  setGravity(gravity)
+}, { immediate: true })
+
 const { onBeforeRender } = useLoop()
 
 onBeforeRender(() => {
