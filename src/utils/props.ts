9532f92a3e8bce786c03b0d061fe2be133648b24
Nathan Mande <neodarksoulink@gmail.com>
2024-09-24 17:53:35 +0200

    refactor(core): watcher types accessibility
    ### Description
    
    - Add object types support
      - `NonNever`
      - `Methods`
      -  `CallableProps`
    - Improve `makePropWatcherRB` types accessibility
    - Improve `makePropsWatcherRB` types accessibility
    
    Co-Authored-By: Jaime A Torrealba C <63722373+JaimeTorrealba@users.noreply.github.com>

--- src/utils/props.ts
+++ src/utils/props.ts
@@ -1,21 +1,30 @@
 import { watch } from 'vue'
+import type { RigidBody } from '@dimforge/rapier3d-compat'
+import type { ShallowRef } from 'vue'
+import type { CallableProps, Methods, RigidBodyContext, RigidBodyProps } from '../types'
 
-export const makePropWatcherRB = (
-  props: any,
-  toWatch: string,
-  instance: any,
-  onSet: string,
+export const makePropWatcherRB = <
+  K extends keyof RigidBodyProps,
+>(
+  props: RigidBodyProps,
+  toWatch: K,
+  instance: ShallowRef<RigidBodyContext['rigidBody']>,
+  onSet: `set${Capitalize<keyof RigidBodyProps>}`,
 ) => watch([() => props[toWatch], instance], ([newValue, _]) => {
   if (!instance.value) { return }
-  instance.value[onSet](newValue, true)
+  ((instance.value[onSet as keyof Methods<RigidBody>]) as CallableProps<RigidBody>[keyof CallableProps<RigidBody>])?.(newValue, true)
 })
 
-export const makePropsWatcherRB = (
-  props: any,
-  watchers: string[],
-  instance: any,
-) => watchers.forEach((watcher) => {
+export const makePropsWatcherRB = <
+  K extends keyof RigidBodyProps,
+>(
+  props: RigidBodyProps,
+  watchers: K[],
+  instance: ShallowRef<RigidBodyContext['rigidBody']>,
+) => watchers.forEach((_) => {
+  const watcher = _ as string
   // Uppercase only for the first letter in the watcher
-  const watcherName = watcher.charAt(0).toUpperCase() + watcher.slice(1)
-  makePropWatcherRB(props, watcher, instance, `set${watcherName}`)
+  const watcherName = watcher.charAt(0).toUpperCase()
+    + watcher.slice(1) as Capitalize<keyof RigidBodyProps>
+  makePropWatcherRB(props, watcher as keyof RigidBodyProps, instance, `set${watcherName}`)
 })
