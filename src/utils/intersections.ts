9a224021741f39f0f4f7fa9437aaed5876747b1c
Nathan Mande <neodarksoulink@gmail.com>
2024-10-11 23:28:25 +0200

    feat: implement `intersection` feature

---
+++ src/utils/intersections.ts
@@ -0,0 +1,12 @@
+import type { collisionType, sourceTarget } from '../types'
+
+export const emitIntersection = (
+  source: sourceTarget,
+  target: sourceTarget,
+  started: boolean,
+) => {
+  const collisionType: collisionType = started ? 'enter' : 'exit'
+  const colliderNode = (source.object as any)?.__vnode?.children?.[1]?.children?.find((child: any) => child?.component?.exposed?.instance?.value === source.context.collider)
+
+  colliderNode?.component?.emit?.(`intersection-${collisionType}`, { source, target })
+}
